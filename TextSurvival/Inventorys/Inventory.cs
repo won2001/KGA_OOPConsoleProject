4c761b47242a9ab612c0dfb7ea4f447a1f6acc03
won2001 <djdjvlf2001@naver.com>
2024-08-16 02:06:01 +0900

    Players_ correction
    players interface 구현을 위한 수정

--- TextSurvival/Inventorys/Inventory.cs
+++ TextSurvival/Inventorys/Inventory.cs
@@ -9,17 +9,61 @@ namespace TextSurvival.Inventorys
 {
     public class Inventory
     {
+        public const int MAX_ITEM = 6;
+
         private List<Item> items;
-        public const int inventorySize = 9;
 
         public Inventory()
         {
-            items = new(inventorySize);
+            items = new List<Item>(MAX_ITEM);
         }
 
-        public void AddItem(Item item)
+        public int ItemCount => items.Count;
+
+        public bool AddItem(Item item)
         {
+            if (items.Count >= MAX_ITEM)
+            {
+                Console.WriteLine("아이템이 가득차서 넣지 못합니다.");
+                return false;
+            }
+
             items.Add(item);
+            return true;
+        }
+
+        public bool RemoveItem(Item item)
+        {
+            return items.Remove(item);
+        }
+
+        public bool RemoveItem(int index)
+        {
+            if (index < 0 || items.Count <= index)
+            {
+                Console.WriteLine("범위를 벗어나는 아이템을 삭제할 순 없습니다.");
+                return false;
+            }
+
+            items.RemoveAt(index);
+            return true;
+        }
+
+        public Item GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void ShowAllItem()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {items[i].name}");
+                Console.WriteLine($" 공격력 : {items[i].attack}");
+                Console.WriteLine($" 설명 : {items[i].description}");
+                Console.WriteLine($" 효과 : {items[i].effect}"); 
+                Console.WriteLine();
+            }
         }
     }
 }
