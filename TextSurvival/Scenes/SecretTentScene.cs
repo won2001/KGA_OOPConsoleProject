709e53d6265787c72b48a0d3e62f857e2ea908d9
won2001 <djdjvlf2001@naver.com>
2024-08-16 16:52:43 +0900

    AbandonedCityScene
    폐도시 구현

--- TextSurvival/Scenes/SecretTentScene.cs
+++ TextSurvival/Scenes/SecretTentScene.cs
@@ -17,8 +17,8 @@ namespace TextSurvival.Scenes
         private Shop shop;
         private Player player;
         private bool isRunning;
-        enum ShopScene { Menu, Buy, Sell};
-        ShopScene shopScene = ShopScene.Menu;
+        enum ShopScene { Buy, Sell};
+        ShopScene shopScene;
         public SecretTentScene(Game game) : base(game)
         {
             isRunning = true;
@@ -34,30 +34,12 @@ namespace TextSurvival.Scenes
         public override void Exit() 
         {
         }
-        public override void Input()
-        {
-            inputKey = Console.ReadKey().Key;
-        }
-        public override void Render()
-        {
-            Console.Clear();
-            Console.WriteLine("건장한 잡상인이 있다.");
-            Console.WriteLine("무엇을 하시겠습니까?");
-            Console.WriteLine("1. 들어간다");
-            
-            Console.Write("선택 : ");
-
-        }
-
-        public override void Update()
+        public void ShopRun()
         {
             while (isRunning)
             {
                 switch (shopScene)
                 {
-                    case ShopScene.Menu:
-                        MenuUpdate();
-                        break;
                     case ShopScene.Buy:
                         BuyUpdate();
                         break;
@@ -67,31 +49,27 @@ namespace TextSurvival.Scenes
                 }
             }
         }
-        public void MenuUpdate()
+        public override void Input()
         {
-            if (input == 0)
-            {
-            }
-            else
-            {
-                Console.Write("아이템 구매");
-            }
-            if (input == 1)
-            {
-            }
-            else
-            {
-                Console.WriteLine("아이템 판매");
-            }
-            if (input == 2)
-            {
+            inputKey = Console.ReadKey().Key;
+        }
+        public override void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("********************");
+            Console.WriteLine("*    아이템 상점   *");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+            Console.WriteLine("======= 상점 메뉴 =======");
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
+            Console.WriteLine("3. 나간다.");
+            Console.Write("무엇을 하시겠습니까? : ");
 
-            }
-            else
-            {
-                Console.WriteLine("나가기");
-            }
+        }
 
+        public override void Update()
+        {
             switch (inputKey)
             {
                 case ConsoleKey.D1:
@@ -110,7 +88,7 @@ namespace TextSurvival.Scenes
                     break;
             }
         }
-        private void BuyUpdate()
+        public void BuyUpdate()
         {
             Console.Clear();
             Console.WriteLine("====== 아이템 구매 ======");
@@ -118,17 +96,15 @@ namespace TextSurvival.Scenes
 
             shop.ShowAllItem();
 
-            Console.Write("구매할 아이템을 선택하세요 (뒤로가기 0) : ");
+            Console.Write("구매할 아이템을 선택하세요 : ");
 
-            // Input();
-            
+            inputKey = Console.ReadKey().Key;
             Console.WriteLine();
             int inputValue = (int)inputKey - (int)ConsoleKey.D0;
 
-            // Update();
             if (inputKey == ConsoleKey.D0)
             {
-                shopScene = ShopScene.Menu;
+                game.ChangeScene(SceneType.SecretTent);
             }
             else if (1 <= inputValue && inputValue <= shop.ItemCount)
             {
@@ -141,7 +117,8 @@ namespace TextSurvival.Scenes
             }
         }
 
-        private void SellUpdate()
+
+        public void SellUpdate()
         {
             Console.Clear();
             Console.WriteLine("====== 아이템 판매 ======");
@@ -149,17 +126,14 @@ namespace TextSurvival.Scenes
 
             player.ShowInventory();
 
-            Console.Write("판매할 아이템을 선택하세요 (뒤로가기 0) : ");
-
-            // Input();
+            Console.Write("판매할 아이템을 선택하세요 : ");
             
             Console.WriteLine();
             int inputValue = (int)inputKey - (int)ConsoleKey.D0;
 
-            // Update();
             if (inputKey == ConsoleKey.D0)
             {
-                shopScene = ShopScene.Menu;
+                game.ChangeScene(SceneType.SecretTent);
             }
             else if (1 <= inputValue && inputValue <= player.InvenCount)
             {
